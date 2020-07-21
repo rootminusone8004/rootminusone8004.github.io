811f2ed335e5ee1f77c97bac48b9e04f31c381aa
rootminusone8004 <45507863+rootminusone8004@users.noreply.github.com>
2020-07-21 12:08:22 +0600

    Update SpeakGoodBye.js

--- cousera/module4-solution/SpeakGoodBye.js
+++ cousera/module4-solution/SpeakGoodBye.js
@@ -14,12 +14,12 @@
 	var speakWord = "Good Bye";
 
 	var byeSpeaker = {
-		speak(name){
-			console.log(speakWord + " " + name);}}
+		
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-
+		speak(name){
+			console.log(speakWord + " " + name);}}
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
 // xxxx.xxxx = byeSpeaker;
